d376de3c9fba37bfee4df2e098368166d4a59c11
Kacper <kacper-cyganik@o2.pl>
2022-04-04 22:09:56 +0200

    Create html base, lobby and room page.

--- core/chat/urls.py
+++ core/chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.lobby, name='lobby'),
+    path('room/', views.room, name='room'),
 ]
--- core/chat/views.py
+++ core/chat/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'chat/home.html', {})
+def lobby(request):
+    return render(request, 'chat/lobby.html', {})
+
+def room(request):
+    return render(request, 'chat/room.html', {})
