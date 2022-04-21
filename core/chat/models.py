d8b4298fb0dd14a9361eb6a0a86586c30e6e4882
Kacper <kacper-cyganik@o2.pl>
2022-04-21 21:54:03 +0200

    Add RoomMember model.

--- core/chat/models.py
+++ core/chat/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class RoomMember(models.Model):
+    name = models.CharField(max_length=255)
+    uid = models.CharField(max_length=255)
+    room_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
