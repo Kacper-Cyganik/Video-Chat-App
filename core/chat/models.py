853d18344f96c5e44e5ad4eac42e3f11225af530
Kacper <kacper-cyganik@o2.pl>
2022-04-22 19:51:56 +0200

    Fix typo.

--- core/chat/models.py
+++ core/chat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class RoomMember(models.Model):
+    
     name = models.CharField(max_length=255)
     uid = models.CharField(max_length=255)
     room_name = models.CharField(max_length=255)
